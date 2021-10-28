8c9c0a1703e79ec9d8f664201bb6e0f44a2db83f
Gerson santos <jhxn07@gmail.com>
2021-10-27 20:03:39 -0500

    HOLA MUNDO

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, illum quidem minus enim dignissimos ut voluptates doloribus, dolor in dicta necessitatibus? Optio provident corrupti, animi dolorem dolore voluptates fugit harum.</p>
+    <h1>{{ msg }}</h1> 
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
