46d5ea06c931ff1f7ec20ac24079714c6ba701a9
Gerson santos <jhxn07@gmail.com>
2021-10-27 21:36:42 -0500

    colors

--- src/components/Micomponente.vue
+++ src/components/Micomponente.vue
@@ -3,6 +3,9 @@
         <h1>Mi primer componente</h1>
         <h2>{{sub}}</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatem commodi quasi, impedit dicta similique eius officiis ab quia quos esse maiores quas eveniet voluptates, fugit sequi facilis tempore! Impedit.</p>
+        <form action="">
+            <input type="color" name="coloreate" id="colors">
+        </form>
     </div>
 </template>
 <script>
