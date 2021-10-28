5ac9cb2203ae67258e585062236116a002098050
Gerson santos <jhxn07@gmail.com>
2021-10-27 20:51:13 -0500

    CREACION DE COMPONENTE

---
+++ src/components/Micomponente.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Mi primer componente</h1>
+        <h2>{{sub}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name:"Micomponente",
+    data() {
+        return{
+            sub: "El primer subtitulo de un componente"
+            }
+    },
+}
+</script>
